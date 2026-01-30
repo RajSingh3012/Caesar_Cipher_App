6a5e88d2dbb0d50c5d3f33f88a14cac41e2edf25
Raj <rajvijaysingh2003@gmail.com>
2026-01-30 15:45:14 -0600

    Fixed Spelling of Caesar

--- webapp.py
+++ webapp.py
@@ -2,11 +2,11 @@ import streamlit as st
 from logo import logo
 from main import caesar
 st.text(logo)
-st.title("Welcome to Ceaser Cipher")
+st.title("Welcome to Caesar Cipher")
 direction = st.selectbox("Type 'encode' to encrypt, type 'decode' to decrypt", ["encode", "decode"])
 text = st.text_input("Type your message")
 shift = st.number_input("Type the shift number", min_value=1, step=1)
-run_cipher = st.button("Cipher it")
+run_cipher = st.button("Cipher It")
 if run_cipher:
     result = caesar(text.lower(), shift, direction.lower())
     st.write(f"Results: {result}")
